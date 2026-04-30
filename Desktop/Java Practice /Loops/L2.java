6ce37e6d4c7253c76444ccc27d56eca73f78ec68
Harsh Kumar <harshkumar011045@gmail.com>
2026-04-30 22:44:38 +0530

    added

---
+++ Desktop/Java Practice /Loops/L2.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class L2 {
+    public static void main(String[] args){
+        int i;
+        int sum =0;
+        for( i=1; i<=200; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+
+    }
+}
